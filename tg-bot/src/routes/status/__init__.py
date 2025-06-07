421f91a47edb55f2510887f4232e962044eb725a
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-07 21:20:13 +0300

    handle status signal

--- tg-bot/src/routes/status/__init__.py
+++ tg-bot/src/routes/status/__init__.py
@@ -1,12 +1,19 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types.callback_query import CallbackQuery
 
 from src.fsm import UserState
 
 router = Router()
 
 
+@router.callback_query(UserState.default, F.data == "status")
+async def query_status(query: CallbackQuery) -> None:
+    assert query.message is not None
+    await query.message.answer("received a /status")
+
+
 @router.message(UserState.default, Command("status"))
 async def command_status(message: Message) -> None:
-    await message.answer("received /status")
+    await message.answer("received a /status")
