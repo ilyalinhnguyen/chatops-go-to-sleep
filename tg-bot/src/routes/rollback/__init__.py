96afddc9f6683caf22e1fee12c3a8bf5a0cd92fb
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-07 18:26:39 +0300

    parse rollback data

--- tg-bot/src/routes/rollback/__init__.py
+++ tg-bot/src/routes/rollback/__init__.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -7,15 +9,31 @@ from src.fsm import UserState
 router = Router()
 
 
+@dataclass(kw_only=True)
+class RollbackData:
+    version: str
+
+    @staticmethod
+    def parse_command(text: str) -> "RollbackData | None":
+        tokens = text.split()
+
+        if len(tokens) != 2:
+            return None
+
+        if tokens[0] != "/rollback":
+            return None
+
+        return RollbackData(version=tokens[1])
+
+
 @router.message(UserState.default, Command("rollback"))
 async def command_rollback(message: Message) -> None:
     assert message.text is not None
 
-    tokens = message.text.split()
+    rollback_data = RollbackData.parse_command(message.text)
+    if rollback_data is None:
+        raise NotImplementedError
+        return
 
-    assert len(tokens) == 2
-    assert tokens[0] == "/rollback"
-
-    version = tokens[1]
-
-    await message.reply(f"received /rollback {version}")
+    await message.reply(f"received /rollback {rollback_data.version}")
+    raise NotImplementedError
