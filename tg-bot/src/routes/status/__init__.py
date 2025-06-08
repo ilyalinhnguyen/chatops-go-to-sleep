0a57c9dbf5f4992a58c0d92c5b64c81dc3f93be8
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-08 12:22:57 +0300

    stupid status

--- tg-bot/src/routes/status/__init__.py
+++ tg-bot/src/routes/status/__init__.py
@@ -1,11 +1,37 @@
+from dataclasses import dataclass
+
 from aiogram import F, Router
+from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.types.callback_query import CallbackQuery
-
+from src import api
 from src.fsm import UserState
 
 router = Router()
+
+
+@dataclass(kw_only=True)
+class StatusData:
+    namespace: str
+    name: str
+
+    @staticmethod
+    def parse_command(text: str) -> "StatusData | None":
+        tokens = text.split()
+
+        if len(tokens) != 2:
+            return None
+
+        if tokens[0] != "/status":
+            return None
+
+        args = tokens[1].split(":")
+
+        if len(args) != 2:
+            return None
+
+        return StatusData(namespace=args[0], name=args[1])
 
 
 @router.callback_query(UserState.default, F.data == "status")
@@ -16,4 +42,19 @@ async def query_status(query: CallbackQuery) -> None:
 
 @router.message(UserState.default, Command("status"))
 async def command_status(message: Message) -> None:
-    await message.answer("received a /status")
+    assert message.text is not None
+
+    status_data = StatusData.parse_command(message.text)
+    if status_data is None:
+        await message.answer(
+            "`/status <NAMESPACE>:<NAME>`",
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
+        return
+
+    response = api.v1.kubernetes.service.status(status_data.namespace, status_data.name)
+    if response is None:
+        await message.answer("Internal error.")
+        return
+
+    await message.answer(str(response["data"]))
