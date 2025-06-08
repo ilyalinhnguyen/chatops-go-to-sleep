8d2ba6a365f9e1f64ec7ece554bbb0cb9c9512a4
ilyalinhnguyen <ilyalinhnguyen@gmail.com>
2025-06-08 16:19:29 +0300

    feat: update metrics

--- tg-bot/src/routes/metrics/__init__.py
+++ tg-bot/src/routes/metrics/__init__.py
@@ -1,14 +1,17 @@
 from aiogram import Router
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from src import api
 from src.fsm import UserState
-
+import json
+from aiogram.enums import ParseMode
+from src.routes import start
 router = Router()
 
 
 @router.message(UserState.default, Command("metrics"))
-async def command_restart(message: Message) -> None:
+async def command_restart(message: Message, state:FSMContext) -> None:
     assert message.text is not None
 
     response = api.v1.prometheus.metrics.basic()
@@ -16,4 +19,8 @@ async def command_restart(message: Message) -> None:
         await message.answer("Internal error.")
         return
 
-    await message.answer(str(response))
+    await message.answer(
+        f"```json\n{json.dumps(response, indent=2)[:4000]}```",
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
+    await start.command_start(message, state)
