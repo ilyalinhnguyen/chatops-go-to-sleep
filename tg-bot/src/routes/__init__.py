d333f0675d4c6a5243ab5b6ebfec303711105353
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-08 14:57:40 +0300

    nodes and pods in router

--- tg-bot/src/routes/__init__.py
+++ tg-bot/src/routes/__init__.py
@@ -1,12 +1,25 @@
 from aiogram import Router
 
-from . import metrics, namespaces, restart, rollback, scale, start, status, update
+from . import (
+    metrics,
+    namespaces,
+    nodes,
+    pods,
+    restart,
+    rollback,
+    scale,
+    start,
+    status,
+    update,
+)
 
 router = Router()
 router.include_routers(
     start.router,
     metrics.router,
     namespaces.router,
+    nodes.router,
+    pods.router,
     restart.router,
     rollback.router,
     scale.router,
