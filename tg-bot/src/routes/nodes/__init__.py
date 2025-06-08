205e1f9850e830c8dae3ce667196f04224edcc6a
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-08 15:23:28 +0300

    /nodes

--- tg-bot/src/routes/nodes/__init__.py
+++ tg-bot/src/routes/nodes/__init__.py
@@ -1,3 +1,25 @@
+import json
+
 from aiogram import Router
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import Message
+from src import api
+from src.fsm import UserState
 
 router = Router()
+
+
+@router.message(UserState.default, Command("nodes"))
+async def command_nodes(message: Message) -> None:
+    assert message.text is not None
+
+    response = api.v1.kubernetes.metrics.nodes()
+    if response is None:
+        await message.answer("Internal error.")
+        return
+
+    await message.answer(
+        f"```json\n{json.dumps(response, indent=2)[:4000]}```",
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
